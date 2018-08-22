42dcbad45788205abf7a4bd35127e8ad553359a0
untaek <untaek@iMac-3.local>
2018-08-22 19:13:31 +0900

    delete un nessesary build(.js) files

--- build/client/util.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/* tslint:disable no-var-requires no-bitwise */
-const flagEmoji = require('emoji-flags');
-exports.flag = (country) => flagEmoji.countryCode(country).emoji;
-exports.randomColor = () => '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
--- build/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-const main = () => {
-    const server = new server_1.Server();
-    server.start();
-};
-main();
